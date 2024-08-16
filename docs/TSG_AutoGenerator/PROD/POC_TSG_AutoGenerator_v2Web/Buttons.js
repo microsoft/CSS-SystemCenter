859661f593e9c7ce683371ecbea4ecd32e7dc35f
khusmeno <khusmeno@hotmail.com>
2024-08-16 15:50:07 +0300

    PROD v2024.08.16.1545

--- docs/TSG_AutoGenerator/PROD/POC_TSG_AutoGenerator_v2Web/Buttons.js
+++ docs/TSG_AutoGenerator/PROD/POC_TSG_AutoGenerator_v2Web/Buttons.js
@@ -10,18 +10,15 @@
     console.log(bodyText);
 
     // Extra phrase to be added
-    const extraPhrase = ":::template /.templates/Common-Header.md\n:::\n\n::: template /.templates/Sandbox-Header.md\n:::\n\n";
+    var extraPhrase = ":::template /.templates/Common-Header.md\n:::\n\n::: template /.templates/Sandbox-Header.md\n:::\n\n";
 
     // Concatenate all the text
-    const clipboardText = `${extraPhrase}${bodyText}`;
-
-    openBrowser();
-
+    var clipboardText = `${extraPhrase}${bodyText}`;
 
     // Attempt to use the Clipboard API
     if (navigator.clipboard && window.isSecureContext) {
         navigator.clipboard.writeText(clipboardText).then(function () {
-            alert('Details copied to clipboard!');
+            console.log('Details copied to clipboard!');
         }).catch(function (err) {
             console.error('Failed to copy text: ', err);
             fallbackCopyTextToClipboard(clipboardText);
@@ -30,7 +27,7 @@
         // Fallback method for older browsers or when Clipboard API is blocked
         fallbackCopyTextToClipboard(clipboardText);
     }
-
+   // openBrowser(); //conflict
 }
 
 
@@ -72,7 +69,7 @@ function fallbackCopyTextToClipboard(text) {
     try {
         const successful = document.execCommand('copy');
         const msg = successful ? 'Details copied to clipboard!' : 'Failed to copy text';
-        alert(msg);
+        console.log(msg);
     } catch (err) {
         console.error('Fallback: Failed to copy text: ', err);
     }
@@ -108,6 +105,69 @@ function openBrowser() {
     }   
 }
 
-window.onload = function () {
-    document.querySelector("button").addEventListener("click", copyToClipboard);
-};
+
+function copyAsPowerShell(){
+    var bodyText = document.getElementById('item-body').innerHTML;
+    bodyText = bodyText.replace(/<o:p>|<\/o:p>/g, ''); //special case where those elements are injected in order to enable Word to convert the HTML back to fully compatible Word document, with everything preserved.
+    //bodyText = bodyText.replace(/(Issue|Issues|Symptom|Symptoms):/g, '<b>$1:</b>'); //special case to add bold for Issue
+    bodyText = bodyText.replace(/(Issue|Issues|Symptom|Symptoms):/g, '<p class="x_wordsection1" style="margin:0in"><b>$1:</b>'); //special case to add bold for Issue
+    bodyText = bodyText.replace(/color:#\d{6};/g, 'color:black;'); //special case for "color:#171717;"
+
+    //fromat HTML for Word alike e.g. pretty print
+    bodyText = formatHTML(bodyText);
+    console.log(bodyText);
+
+    // Extra phrase to be added
+    var extraPhrase = ":::template /.templates/Common-Header.md\n:::\n\n::: template /.templates/Sandbox-Header.md\n:::\n\n";
+
+    // Concatenate all the text
+    var clipboardText = `
+$productName = "SCORCH"
+$aliasName = "Daniel"
+$subPageTitle = "test wiki submitted via ADO API"
+$subPageBody =
+@'
+${extraPhrase}
+${bodyText}
+'@
+##########################################################################################################################
+#region Azure CLI is required to get an ADO token
+if (-not (get-command az)) {
+    $msgStr = "Azure CLI has to be installed. Run the below in an ELEVATED PS window, open a new PS instance and then re - run this script."
+    $msgStr += "    $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindowsx64 -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; Remove-Item .\AzureCLI.msi"
+    Write - Error $msgStr
+    return
+}
+#endregion
+##########################################################################################################################
+
+$accessTokenJson = az account get-access-token | ConvertFrom-Json
+$bearerTokenId = $accessTokenJson.accessToken
+$adoHeaders = @{
+    'Authorization' = "bearer $bearerTokenId"
+    "accept"= "application/json;api-version=5.0-preview.1;excludeUrls=true;enumsAsNumbers=true;msDateFormat=true;noArrayWrap=true"
+}
+$wikiUrl = "https://dev.azure.com/supportability/AzureMonitor/_apis/wiki/wikis/AzureMonitor.wiki/pages?path=/Sandbox/Hybrid%20Sandbox/$productName/$aliasName/$subPageTitle&api-version=7.1-preview.1"
+
+$payload = @'
+{
+    "content": "|||subPageBodyInPayLOAD|||"
+}
+'@
+$payload = $payload.Replace("|||subPageBodyInPayLOAD|||", $subPageBody.Replace('"', '\"'))
+Invoke-RestMethod -Uri $wikiUrl -Method Put -ContentType "application/json" -Headers $adoHeaders -Body $payload
+`;
+
+    // Attempt to use the Clipboard API
+    if (navigator.clipboard && window.isSecureContext) {
+        navigator.clipboard.writeText(clipboardText).then(function () {
+            console.log('Details copied to clipboard!');
+        }).catch(function (err) {
+            console.error('Failed to copy text: ', err);
+            fallbackCopyTextToClipboard(clipboardText);
+        });
+    } else {
+        // Fallback method for older browsers or when Clipboard API is blocked
+        fallbackCopyTextToClipboard(clipboardText);
+    }
+}
